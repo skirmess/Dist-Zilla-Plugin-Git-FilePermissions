7524bb96f5dd267a252405e54b300e30c57e4c18
Sven Kirmess <sven.kirmess@kzone.ch>
2017-10-21 01:39:58 +0200

    0.003

--- lib/Dist/Zilla/Plugin/Git/FilePermissions.pm
+++ lib/Dist/Zilla/Plugin/Git/FilePermissions.pm
@@ -4,7 +4,7 @@ use 5.006;
 use strict;
 use warnings;
 
-our $VERSION = '0.002';
+our $VERSION = '0.003';
 
 use Moose;
 
@@ -148,7 +148,7 @@ Dist::Zilla::Plugin::Git::FilePermissions - fix the file permissions in your Git
 
 =head1 VERSION
 
-Version 0.002
+Version 0.003
 
 =head1 SYNOPSIS
 
